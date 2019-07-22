39fba7721217b7eb1fbb9ac149a3b06a15589531
mac <mac@DN0a0fbe0b.SUNet>
2019-07-21 21:28:55 -0700

    contact features

--- src/pages/submission.js
+++ src/pages/submission.js
@@ -5,6 +5,10 @@ import SEO from "../components/seo";
 
 function Submission() {
   return (
+  	<SEO
+        title="Success"
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+      />
     <Layout>
       <div>
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
