011a8a18987af8e6704b45637684b2e389f15f0b
mac <mac@DN0a0fbe0b.SUNet>
2019-07-21 21:00:34 -0700

    hope this contact form works

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,65 +8,6 @@ function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-            <form name = "contact" method = "POST" data-netlify="true" className="mx-auto md:w-1/2">
-          <p className="leading-loose mb-8">
-            Here is an example of a form built using Tailwind.{" "}
-            More form examples are available <a
-
-              href="https://tailwindcss.com/docs/examples/forms"
-              className="font-bold no-underline text-gray-700"
-            >here</a>.
-
-          </p>
-          <label
-            className="block font-bold mb-2 text-xs uppercase"
-            htmlFor="first-name"
-          >
-            Name
-          </label>
-
-          <input
-            className="appearance-none block bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
-            id="first-name"
-            type="text"
-            name="name"
-            placeholder="Bill"
-          />
-          <label
-            className="block font-bold mb-2 text-xs uppercase"
-            htmlFor="last-name"
-          >
-            Email
-          </label>
-          <input
-            className="appearance-none block bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
-            id="last-name"
-            type="email"
-            placeholder="me@gmail.com"
-            name="email"
-          />
-          <label
-            className="block font-bold mb-2 text-xs uppercase"
-            htmlFor="message"
-          >
-            Message
-          </label>
-
-          <textarea
-            className="appearance-none bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
-            id="message"
-            placeholder="Say something..."
-            rows="8"
-            name="message"
-          />
-
-          <button
-            type = "submit"
-            className=" border-gray-800 hover:border-gray-700 bg-gray-700 hover:bg-gray-600 font-bold px-4 py-2 rounded text-sm text-white">
-            Submit
-          </button>
-        </form> 
-
       <div>
         <img
           src={abductionIllustration}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,20 +11,68 @@ function ContactPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
       <section>
-        <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>  
+
+       <form name = "contact" method = "POST" data-netlify="true" data-netlify-honeypot="bot-field" className="mx-auto md:w-1/2">
+          <input type="hidden" name="bot-field" />
+          <p className="leading-loose mb-8">
+            Here is an example of a form built using Tailwind.{" "}
+            More form examples are available <a
+
+              href="https://tailwindcss.com/docs/examples/forms"
+              className="font-bold no-underline text-gray-700"
+            >here</a>.
+
+          </p>
+          <label
+            className="block font-bold mb-2 text-xs uppercase"
+            htmlFor="first-name"
+          >
+            Name
+          </label>
+
+          <input
+            className="appearance-none block bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
+            id="first-name"
+            type="text"
+            name="name"
+            placeholder="Bill"
+          />
+          <label
+            className="block font-bold mb-2 text-xs uppercase"
+          >
+            Email
+          </label>
+          <input
+            className="appearance-none block bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
+            id="last-name"
+            type="email"
+            placeholder="me@gmail.com"
+            name="email"
+          />
+          <label
+            className="block font-bold mb-2 text-xs uppercase"
+            htmlFor="message"
+          >
+            Message
+          </label>
+
+          <textarea
+            className="appearance-none bg-gray-200 mb-6 px-3 py-2 rounded-md text-gray-700 w-full"
+            id="message"
+            placeholder="Say something..."
+            rows="8"
+            name="message"
+          />
+
+          <button
+            type = "submit"
+            className=" border-gray-800 hover:border-gray-700 bg-gray-700 hover:bg-gray-600 font-bold px-4 py-2 rounded text-sm text-white">
+            Submit
+          </button>
+
+
+        </form> 
+  
       </section>
     </Layout>
   );
