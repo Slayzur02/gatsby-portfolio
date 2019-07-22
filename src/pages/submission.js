2c6755401cc2348d3e2e6590a7fca5bcac9a1552
mac <mac@DN0a0fb6b6.SUNet>
2019-07-21 22:32:51 -0700

    haha this netlify commit takes wayy too long

--- src/pages/submission.js
+++ src/pages/submission.js
@@ -5,11 +5,11 @@ import SEO from "../components/seo";
 
 function Submission() {
   return (
-  	<SEO
+    <Layout>
+    <SEO
         title="Success"
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
-    <Layout>
       <div>
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
           Some of my aliens just fetched your submissions... Great!
